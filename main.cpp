de4abfb8faefa4ffbd18cca2f764c0067883920a
Hanoch Griner <eugriner@gmail.com>
2016-02-12 10:49:22 -0300

    FIX - Padronizacao do layout

--- main.cpp
+++ main.cpp
@@ -6,8 +6,13 @@ int main(int argc, char *argv[])
     //#if QT_VERSION < QT_VERSION_CHECK(5, 0, 0)
       //  QApplication::setGraphicsSystem("raster");
     //#endif
-    //QApplication::setStyle("fusion");
+    QApplication::setStyle("fusion");
+
     QApplication a(argc, argv);
+
+    QFont newFont("Ubuntu", 10, 0, true);
+    QApplication::setFont(newFont);
+
     supervisorio w;
     //Controle c;
     w.show();
