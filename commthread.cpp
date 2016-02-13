542f7e205c8b2f0302d105704a70a997d8a07069
danielholanda <holand.daniel@gmail.com>
2016-02-12 23:08:45 -0300

    Sugestoes de implementacao

--- commthread.cpp
+++ commthread.cpp
@@ -14,8 +14,15 @@ void commThread::run(){
         emit waterLevelRead(i*0.25,i*0.5); //Envia sinal do nível dos tanques para ser lido por um slot do supervisório
         this->msleep(100);
     }
+    /* Aqui deve ser lido o tempo a todo instante e, somente quando decorrido 0.1s, a comunicação acontece
+     * Esse 'for loop' acima é apenas para demonstrar o funcionamento da comunicação entre thread e supervisório */
 }
 
 void commThread::OnOutputVoltageChanged(double voltageTank1, double voltageTank2){
     //Recebe o sinal do supervisório nível de tensão a ser enviado para os tanques
+
+    /* Esse sinal vindo do supervisório chegará a uma taxa bem maior do que a taxa de envio
+     * de comunicação para os tanques. Portanto, aqui devemos apenas armazenar os valores
+     * recebidos para futuramente enviar-los.
+    */
 }
