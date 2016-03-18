40070a0b0473a6aaaa78cb90adcfcccb55f8b4b4
danielholanda <holand.daniel@gmail.com>
2016-03-18 10:35:16 -0300

    ADD - Tool tips das variáveis de análise

--- supervisorio.cpp
+++ supervisorio.cpp
@@ -1,7 +1,9 @@
 #include "supervisorio.h"
 #include "ui_supervisorio.h"
 #include <QtCore/qmath.h>
+#include <QToolTip>
 #include <cmath>
+
 
 
 supervisorio::supervisorio(QWidget *parent) :
@@ -88,6 +90,12 @@ supervisorio::supervisorio(QWidget *parent) :
 
      //Analist calcula valores para relatorios e analise da dinamica
      analist = new Analist();
+
+     //Tool Tips
+     ui->label_tr_0->setToolTip("Tempo de Subida");
+     ui->label_tp_0->setToolTip("Tempo de Pico");
+     ui->label_ts_0->setToolTip("Tempo de Estabilização");
+     ui->label_mp_0->setToolTip("Overshoot");
 }
 
 supervisorio::~supervisorio()
