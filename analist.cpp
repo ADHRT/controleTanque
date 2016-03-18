2a21f50b3e4167ebd3516739166fdd7b42e2ad28
taypi <taynaran.n@gmail.com>
2016-03-18 10:51:23 -0300

    analist improvment

--- analist.cpp
+++ analist.cpp
@@ -5,3 +5,68 @@ Analist::Analist()
 
 }
 
+void Analist::calc(double nivel, double setPoint, double timeStamp)
+{
+
+}
+
+double Analist::getTs()
+{
+    return ts;
+}
+
+double Analist::getMp()
+{
+    return mp;
+}
+
+double Analist::getTp()
+{
+    return tp;
+}
+
+double Analist::getTr()
+{
+    return tr;
+}
+
+void Analist::setTsOpt(int)
+{
+
+}
+
+void Analist::setTrOpt(int)
+{
+
+}
+
+void Analist::reset()
+{
+    ts = 0;
+    mp = 0;
+    tp = 0;
+    tr = 0;
+    tsOldTime = 0;
+    trOldTime = 0;
+}
+
+double Analist::calcTs(double nivel, double setPoint)
+{
+
+}
+
+double Analist::calcMp(double nivel, double setPoint)
+{
+
+}
+
+double Analist::calcTp(double nivel, double setPoint)
+{
+
+}
+
+double Analist::calcTr(double nivel, double setPoint)
+{
+
+}
+
