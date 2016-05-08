73a853642616f9d3770357b70eb96a8fafe18cb8
Hanoch Griner <eugriner@gmail.com>
2016-05-08 14:13:22 -0300

    FIX - 0 warnings

--- supervisorio.cpp
+++ supervisorio.cpp
@@ -918,8 +918,8 @@ void supervisorio::on_pushButton_8_clicked()
     castControl[0] = static_cast<int>(control[0]);
     castControl[1] = static_cast<int>(control[1]);
 
-    bool observador = ui->checkBox_observador_ativar;
-    cThread->setParameters(frequencia, amplitude, offset, duracaoMax, duracaoMin, wave, malha, channel, castControl, kp, ki, kd, windup, conditionalIntegration, taw, tank, cascade, observador ,lOb);
+    bool observer = ui->checkBox_observador_ativar;
+    cThread->setParameters(frequencia, amplitude, offset, duracaoMax, duracaoMin, wave, malha, channel, castControl, kp, ki, kd, windup, conditionalIntegration, taw, tank, cascade, observer ,lOb);
 }
 
 void supervisorio::onPlotValues(double timeStamp, double sinalCalculadoMestre, double sinalCalculadoEscravo, double sinalSaturado, double nivelTanque1, double nivelTanque2, double setPoint, double erro, double iMestre, double iEscravo, double dMestre, double dEscravo){
